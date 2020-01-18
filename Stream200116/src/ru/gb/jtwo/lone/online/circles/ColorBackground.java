e06a9c6fa2c1e550c009156d559686ad39765872
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-01-18 23:06:44 +0300

    Добавление класса ColorBackground

---
+++ Stream200116/src/ru/gb/jtwo/lone/online/circles/ColorBackground.java
@@ -0,0 +1,13 @@
+package ru.gb.jtwo.lone.online.circles;
+
+import java.awt.*;
+
+public class ColorBackground extends Color {
+    private int red;
+    private int green;
+    private int blue;
+
+    public ColorBackground(int red, int green, int blue) {
+        super(red, green, blue);
+    }
+}
