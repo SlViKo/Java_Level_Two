d756db38686d76986e144e1544b38f4025386020
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-01-22 00:14:25 +0300

    Добавлены методы получения из строки массива строки и преобразования массива строк в массив чисел

--- Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/LineWork.java
+++ Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/LineWork.java
@@ -1,12 +1,16 @@
 package ru.gb.jtwo.lone.online.lessontwo;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class LineWork {
     private String line;
+    private int[][] arrInt;
+    private String[][] arrString;
 
     public LineWork(String line) {
         this.line = line;
@@ -15,8 +19,8 @@ public class LineWork {
     public LineWork(File file) {
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
             int ch;
-            while((ch = fileInputStream.read()) != -1) {
-                line += (char)ch;
+            while ((ch = fileInputStream.read()) != -1) {
+                line += (char) ch;
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
@@ -26,12 +30,41 @@ public class LineWork {
 
     }
 
-    public int[][] getIntArray() {
-        
+    public String[][] changeStringToArray() {
+
+        String[] arrOne = line.split("\n");
+        arrString = new String[arrOne.length][arrOne.length];
+        for (int i = 0; i < arrOne.length; i++) {
+            arrString[i] = arrOne[i].split(" "); ;
+        }
+        return arrString;
+    }
+
+    public int[][] changeStringToInt() {
+        if(arrString == null) {
+            this.changeStringToArray();
+        }
+        arrInt = new int[arrString.length][arrString.length];
+        for (int i = 0; i < arrString.length; i++) {
+            for (int j = 0; j < arrString.length; j++) {
+                arrInt[i][j] = Integer.parseInt(arrString[i][j]);
+            }
+        }
+        return arrInt;
+    }
+
+    public int getSumDivideTwo() {
+        int sum=0;
+        for (int i = 0; i < arrInt.length; i++) {
+            for (int j = 0; j < arrInt[i].length; j++) {
+                sum += arrInt[i][j];
+            }
+        }
+        return sum;
     }
 
     @Override
     public String toString() {
-        return line;
+        return Arrays.toString(arrString);
     }
 }
--- Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/StringMain.java
+++ Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/StringMain.java
@@ -6,9 +6,32 @@ public class StringMain {
     public static void main(String[] args) {
 
         String string = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+        System.out.println(string.split("\n").length);
 
         LineWork lineWork = new LineWork(string);
 
+        int[][] arr = lineWork.changeStringToInt();
+        String[][] strings = lineWork.changeStringToArray();
+        int sum = lineWork.getSumDivideTwo();
+
+        System.out.println(sum);
+        System.out.println(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j]+ " ");
+                }
+            System.out.println();
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length; j++) {
+                System.out.print(strings[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         LineWork lineWorkFile = new LineWork(new File(System.getProperty("user.dir"),"example.txt"));
+
     }
 }
