fd6be761bcf2d131af74847da0426976c3295a15
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-01-22 10:29:43 +0300

    исправление методов

--- Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/ArrayFourException.java
+++ Stream200116/src/ru/gb/jtwo/lone/online/lessontwo/ArrayFourException.java
@@ -1,4 +1,18 @@
 package ru.gb.jtwo.lone.online.lessontwo;
 
-public class ArrayFourException {
+public class ArrayFourException extends Exception {
+    private String message = "Ошибка: Массив должен быть размерностью 4 на 4";
+
+    public ArrayFourException() {
+
+    }
+
+    public ArrayFourException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
 }
