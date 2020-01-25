868b003da054d8d3336c65657831957e8e324250
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-01-25 22:26:26 +0300

    Добавление основного класса

--- Stream200116/src/ru/gb/jtwo/lone/online/lessonthree/TestUniqueWordInArray.java
+++ Stream200116/src/ru/gb/jtwo/lone/online/lessonthree/TestUniqueWordInArray.java
@@ -1,4 +1,13 @@
 package ru.gb.jtwo.lone.online.lessonthree;
 
 public class TestUniqueWordInArray {
+    public static void main(String[] args) {
+        String[] animals = new String[] {"Кошка", "Собака", "Мышь", "Овца", "Лощадь", "Кошка", "Курица", "Мышь",
+                                        "Жираф", "Корова", "Собака", "Медведь", "Волк", "Кошка", "Лошадь", "Корова",
+                                        "Кошка", "Волк", "Курица", "Индюк", "Кошка", "Мышь", "Лошадь", "Курица"};
+
+        UniqueWordInArray uniqueWordInArray = new UniqueWordInArray(animals);
+        uniqueWordInArray.printUniqueWord();
+        uniqueWordInArray.printQuantityRepeatWords();
+    }
 }
