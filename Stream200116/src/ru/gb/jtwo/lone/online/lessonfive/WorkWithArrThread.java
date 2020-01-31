818f93c6779c70c7d2c96a5b66101cfa37323ccc
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-01-31 18:35:18 +0300

    Методы для расчета с использованием многопоточности и без

--- Stream200116/src/ru/gb/jtwo/lone/online/lessonfive/WorkWithArrThread.java
+++ Stream200116/src/ru/gb/jtwo/lone/online/lessonfive/WorkWithArrThread.java
@@ -1,32 +1,24 @@
+/**
+ * класс для расчета элементов массива в потоке
+ *
+ * @author SlViKo (Кобозев Вячеслав)
+ * @version date 31/01/2010
+ */
 package ru.gb.jtwo.lone.online.lessonfive;
 
-public class WorkWithArr extends Thread {
+public class WorkWithArrThread extends Thread {
 
     private float[] arr;
 
-
-
-    @Override
-    public void run() {
-
-    }
-
-    public WorkWithArr(float[] arr) {
+    public WorkWithArrThread(float[] arr) {
         this.arr = arr;
     }
 
-
-
-    public void calculationWithoutThread() {
-
-    }
-
-
-    public float[] calculationWithThread(float[] arrPart) {
-        for (int i = 0; i < arrPart.length; i++) {
-            arrPart[i] = (float) (arrPart[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        return arrPart;
     }
 
 
