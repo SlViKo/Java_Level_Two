f60cd7917d05f005ff49447dbc8d044d3e4f6cb3
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-02-23 21:08:08 +0300

    удаление лишнего

--- Stream200116/src/ru/gb/jtwo/lone/online/lessonone/circles/Mashin.java
+++
@@ -1,8 +0,0 @@
-package ru.gb.jtwo.lone.online.lessonone.circles;
-
-import java.awt.*;
-
-public interface Mashin {
-    void update(GameCanvas gameCanvas, float deltaTimt);
-     void render(GameCanvas gameCanvas, Graphics g);
-}
