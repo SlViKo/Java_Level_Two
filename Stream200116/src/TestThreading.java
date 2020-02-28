bf132d112eee758e7eb690fcbe8ef5daa246f78d
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-02-28 22:17:40 +0300

    Методы вывода букв в потоке

--- Stream200116/src/TestThreading.java
+++ Stream200116/src/TestThreading.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+/**
+ *
+ * Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+ *
+ * @author SlViKo(Кобозев Вячеслав)
+ * @version date 28/02/2020
+ */
 public class TestThreading {
+
+    public static void main(String[] args) {
+        PrintCharThread pct = new PrintCharThread();
+
+        Thread a = new Thread(() -> {
+           pct.printA();
+        });
+
+        Thread b = new Thread(() -> {
+            pct.printB();
+        });
+
+        Thread c = new Thread(() -> {
+           pct.printC();
+        });
+
+        a.start();
+        b.start();
+        c.start();
+    }
 }
